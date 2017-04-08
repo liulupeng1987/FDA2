9aa4d9407d134a2006b608d8d37f8d2ef4a4690f
liulupeng <liulupeng3385@163.com>
2017-04-08 10:46:48 +0800

    move sqlite3 to dev group & add pg to production group

---
+++ db/migrate/20170408023943_create_manufacturers.rb
@@ -0,0 +1,15 @@
+class CreateManufacturers < ActiveRecord::Migration[5.0]
+  def change
+    create_table :manufacturers do |t|
+      t.string :name
+      t.text :address
+      t.text :description
+      t.string :contact_person
+      t.string :phone
+      t.string :email
+      t.string :website
+
+      t.timestamps
+    end
+  end
+end
