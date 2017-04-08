a97537a531c211fa0b76689e59c5009ceb79bb29
liulupeng <liulupeng3385@163.com>
2017-04-08 11:44:12 +0800

    install devise

--- app/controllers/manufacturers_controller.rb
+++ app/controllers/manufacturers_controller.rb
@@ -1,48 +1,49 @@
 class ManufacturersController < ApplicationController
-def show
-  @manufacturer = Manufacturer.find(params[:id])
-end
-
-def index
-  @manufacturers = Manufacturer.all
-end
-
-def new
-  @manufacturer = Manufacturer.new
-end
-
-def create
-  @manufacturer = Manufacturer.new(manufacturer_params)
-
-  if @manufacturer.save
-    redirect_to manufacturers_path
-  else
-    render :new
+  before_action :authenticate_user!, only: [:new, :create, :update, :edit, :destroy, :search]
+  def show
+    @manufacturer = Manufacturer.find(params[:id])
   end
-end
 
-def edit
-  @manufacturer = Manufacturer.find(params[:id])
-end
-
-def update
-  @manufacturer = Manufacturer.find(params[:id])
-  if @manufacturer.update(manufacturer_params)
-    redirect_to manufacturers_path
-  else
-    render :edit
+  def index
+    @manufacturers = Manufacturer.all
   end
-end
 
-def destroy
-  @manufacturer = Manufacturer.find(params[:id])
-  @manufacturer.destroy
-  redirect_to manufacturers_path
-end
+  def new
+    @manufacturer = Manufacturer.new
+  end
 
-private
+  def create
+    @manufacturer = Manufacturer.new(manufacturer_params)
 
-def manufacturer_params
-  params.require(:manufacturer).permit(:name, :address, :description, :contact_person, :phone, :email, :website)
-end
+    if @manufacturer.save
+      redirect_to manufacturers_path
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @manufacturer = Manufacturer.find(params[:id])
+  end
+
+  def update
+    @manufacturer = Manufacturer.find(params[:id])
+    if @manufacturer.update(manufacturer_params)
+      redirect_to manufacturers_path
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @manufacturer = Manufacturer.find(params[:id])
+    @manufacturer.destroy
+    redirect_to manufacturers_path
+  end
+
+  private
+
+  def manufacturer_params
+    params.require(:manufacturer).permit(:name, :address, :description, :contact_person, :phone, :email, :website)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users
   root 'manufacturers#index'
   resources :manufacturers
 end
